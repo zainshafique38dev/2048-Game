3dd398cdb59e046bc3b376c6a95d0656ce8ba4c2
zainshafique38dev <zainshafique38@gmail.com>
2025-03-23 13:42:50 +0500

    Update utils.asm
    capture key input func is added

--- gui/utils.asm
+++ gui/utils.asm
@@ -1,23 +1,86 @@
 .386
-.model flat, stdcall
-option casemap:none
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+    ; Define any required data variables here
 
 .code
 
-PUBLIC ConvertHexColor
-
-ConvertHexColor proc color:DWORD
-    mov eax, color   ; EAX = 0x00RRGGBB
-    mov edx, eax     
-    and eax, 0FFh    
+; Existing ConvertHexColor function
+ConvertHexColor PROC
     shl eax, 16      
     and edx, 0FF00h  
     or eax, edx      
+    shr edx, 8       
+    and ecx, 0FF0000h
     mov ecx, color   
     and ecx, 0FF0000h 
     shr ecx, 16      
+    or eax, ecx      ; final format: 0xBBGGRR
     or eax, ecx      ; Combine BBGGRR
-    
     ret
-ConvertHexColor endp
+ConvertHexColor ENDP
+
+; -------------------------------
+; New Function: Capture Key Input
+; -------------------------------
+CaptureKeyInput PROC
+    LOCAL msg:MSG
+    ; Message loop to capture key inputs
+KeyLoop:
+    invoke GetMessage, ADDR msg, 0, 0, 0
+    cmp eax, 0
+    je ExitKeyLoop
+
+    cmp msg.message, WM_KEYDOWN
+    jne KeyLoop
+
+    mov eax, msg.wParam
+
+    cmp eax, 'W'
+    je MoveUp
+    cmp eax, 'A'
+    je MoveLeft
+    cmp eax, 'S'
+    je MoveDown
+    cmp eax, 'D'
+    je MoveRight
+    cmp eax, 'Q'
+    je QuitGame
+    cmp eax, 'R'
+    je ResetGame
+
+    jmp KeyLoop
+
+MoveUp:
+    ; Handle moving up
+    ; Add the required logic here
+    jmp KeyLoop
+
+MoveLeft:
+    ; Handle moving left
+    jmp KeyLoop
+
+MoveDown:
+    ; Handle moving down
+    jmp KeyLoop
+
+MoveRight:
+    ; Handle moving right
+    jmp KeyLoop
+
+QuitGame:
+    ; Handle quitting the game
+    invoke ExitProcess, 0
+
+ResetGame:
+    ; Handle resetting the game
+    jmp KeyLoop
+
+ExitKeyLoop:
+    ret
+CaptureKeyInput ENDP
+
 end
